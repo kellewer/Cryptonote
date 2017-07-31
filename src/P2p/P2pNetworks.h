c268ff7e6e126a97b52a33a3b903c5771f9327f4
kellewer <kellewer@gmail.com>
2017-07-31 13:53:23 -0300

    Old Crypto Code

---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2011-2016 The Cryptonote developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0xcb, 0x25, 0xef, 0xe, 0x31, 0xfb, 0xe, 0x14, 0xd7, 0x14, 0xfb, 0xe, 0x8, 0xcb, 0x25, 0x31 } };
+}
